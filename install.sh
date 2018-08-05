b99bcea786312fd1aec2e71d7fe0aa832e79fa84
raychao <ray20013247777@gmail.com>
2018-08-05 00:04:21 -0700

    add command install basic apt (vim, ssh, samba....)

--- install.sh
+++ install.sh
@@ -14,8 +14,35 @@ show_usage() {
 EOF
 }
 
-while getopts "h" opt; do
-	case $opt in
+while getopts "ahd" opt; do
+	case "$opt" in
+		a) InstallApt=yes;;
 		h) show_usage; exit 0;;
+		d) DEBUG=on;;
 	esac
 done
+
+
+exe() {
+	echo "$1"
+	if [ -z "$DEBUG" ]; then
+		eval "$1"
+	fi
+}
+
+install_apt() {
+	exe "apt update"
+	exe "apt install -y git"
+	exe "apt install -y ssh vim samba"
+	exe "apt install -y build-essential ctags cscope"
+	exe "apt install -y python-pip python"
+	exe "apt install -y python3-pip python3"
+	exe "apt install -y silversearcher-ag id-utils"
+
+	exe "pip3 install --upgrade requests"
+	exe "pip3 install --upgrade zdict"
+}
+
+if [ "$InstallApt" ]; then
+	install_apt
+fi
